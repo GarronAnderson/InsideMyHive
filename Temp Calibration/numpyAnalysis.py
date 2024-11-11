2a11a439a2998d0790e7b7510ead5443dd819dba
Garron Anderson <garronanderson4321@gmail.com>
2024-11-10 20:57:10 -0600

    work on analysis
    work on analysis some, move thermometer
    
    water intrusion incident

---
+++ Temp Calibration/numpyAnalysis.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+#from scipy.interpolate import interp1d
+
+scale_data = np.genfromtxt('hm-scale-trimmed.csv', delimiter=',')
+
+temp_data = np.genfromtxt('hm-temp-trimmed.csv', delimiter=',')
+
+print(temp_data)
+
+scale_data = np.delete(scale_data, 1, 1)
+temp_data = np.delete(temp_data, 1, 1)
+
+print(temp_data)
+
+if len(scale_data) > len(temp_data):
+    scale_data = scale_data[:len(temp_data)]
+else:
+    temp_data = temp_data[:len(scale_data)]
+    
+#mapper = interp1d([min(scale_data), max(scale_data)], [min(temp_data), max(temp_data)])
+    
+#scale_mapped = mapper(scale_data)
+
+
+#plt.plot(temp_data)
+#plt.plot(scale_mapped)
+#plt.show()
+
+plt.scatter(scale_data, temp_data)
+
+plt.show()
--- Temp Calibration/viewFirstLines.py
+++ Temp Calibration/viewFirstLines.py
@@ -1,11 +1,11 @@
 import csv
 
-for filename in ['hm-temp.csv', 'hm-scale.csv']:
+for filename in ['hm-temp-trimmed.csv', 'hm-scale-trimmed.csv']:
     with open(filename, 'r') as file:
         csv_reader = csv.reader(file)
         line_count = 0
         for row in csv_reader:
-            if line_count < 20:
+            if line_count < 10:
                 print(row)
                 line_count += 1
             else:
